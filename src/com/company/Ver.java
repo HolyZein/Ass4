ea4292922fdf4d7145e19e7e776be9b4d7a958b8
HolyZein <84672524+HolyZein@users.noreply.github.com>
2021-06-09 13:21:36 +0600

    Update Ver.java

--- src/com/company/Ver.java
+++ src/com/company/Ver.java
@@ -3,224 +3,169 @@ import java.util.*;
 import java.util.stream.Stream;
 
 
-class Ver<V>{
-    private V data;
-    private Map<Ver<V>, Double> adj = new HashMap<>();
+    class Ver<V>{
+        private final V data;
+        private final Map<Ver<V>, Double> adj = new HashMap<>();
 
-    public Ver(V info) {
-        this.data = info;
+        public Ver(V info) {
+            this.data = info;
+        }
+
+        public void addAdjVer(Ver<V> dest, double weigh){
+            adj.put(dest, weigh);
+        }
+
+        public Map<Ver<V>, Double> getAdj() {
+            return adj;
+        }
+
+        public V getData() {
+            return data;
+        }
     }
 
-    public void addAdjVer(Ver<V> dest, double weigh){
-        adj.put(dest, weigh);
+    class BreadthFirstFind<T> extends Find<T> {
+
+        public BreadthFirstFind(weiGraph<T> gr, T sou) {
+            super(new Ver<>(sou));
+            bfs(gr, sou);
+        }
+
+        private void bfs(weiGraph<T> gr, T cur) {
+            flagged.add(new Ver<>(cur));
+            Queue<Ver<T>> q = new LinkedList<>();
+            q.add(new Ver<>(cur));
+            if (!q.isEmpty()) do {
+                Ver<T> tVer = q.remove();
+                gr.verMap.get(gr.verMap.indexOf(tVer)).getAdj().keySet().stream().filter(ver -> !flagged.contains(ver)).forEach(ver -> {
+                    flagged.add(ver);
+                    verMap.put(ver, tVer);
+                    q.add(ver);
+                });
+            } while (!q.isEmpty());
+        }
     }
 
-    public Map<Ver<V>, Double> getAdj() {
-        return adj;
-    }
+    class DepthFirstFind<T> extends Find<T> {
 
-    public V getData() {
-        return data;
-    }
-}
+        public DepthFirstFind(weiGraph<T> gr, T sou) {
+            super(new Ver<>(sou));
+            dfs(gr, sou);
+        }
 
-class BreadthFirstFind<T> extends Find<T> {
+        private void dfs(weiGraph<T> gr, T curr) {
+            flagged.add(new Ver<>(curr));
+            counter++;
 
-    public BreadthFirstFind(weiGraph<T> gr, T sou) {
-        super(new Ver<>(sou));
-        bfs(gr, sou);
-    }
-
-    private void bfs(weiGraph<T> gr, T cur) {
-        flagged.add(new Ver<>(cur));
-        Queue<Ver<T>> q = new LinkedList<>();
-        q.add(new Ver<>(cur));
-        if (!q.isEmpty()) do {
-            Ver<T> tVer = q.remove();
-            gr.verMap.get(gr.verMap.indexOf(tVer)).getAdj().keySet().stream().filter(ver -> !flagged.contains(ver)).forEach(ver -> {
-                flagged.add(ver);
-                verMap.put(ver, tVer);
-                q.add(ver);
+            Ver<T> cur = new Ver<>(curr);
+            gr.verMap.get(gr.verMap.indexOf(cur)).getAdj().keySet().stream().filter(ver -> !flagged.contains(ver)).forEach(ver -> {
+                verMap.put(ver, new Ver<>(curr));
+                dfs(gr, ver.getData());
             });
-        } while (!q.isEmpty());
-    }
-}
-
-class DepthFirstFind<T> extends Find<T> {
-
-    public DepthFirstFind(weiGraph<T> gr, T sou) {
-        super(new Ver<>(sou));
-        dfs(gr, sou);
-    }
-
-    private void dfs(weiGraph<T> gr, T curr) {
-        flagged.add(new Ver<>(curr));
-        counter++;
-
-        Ver<T> cur = new Ver<>(curr);
-        gr.verMap.get(gr.verMap.indexOf(cur)).getAdj().keySet().stream().filter(ver -> !flagged.contains(ver)).forEach(ver -> {
-            verMap.put(ver, new Ver<>(curr));
-            dfs(gr, ver.getData());
-        });
-    }
-
-}
-
-class Find<T> {
-    protected int counter;
-    protected Set<Ver<T>> flagged;
-    protected Map<Ver<T>, Ver<T>> verMap;
-    protected Ver<T> sou;
-
-    public Find(Ver<T> sou) {
-        this.sou = sou;
-        flagged = new HashSet<>();
-        verMap = new HashMap<>();
-    }
-
-    public boolean ifHas(Ver<T> v) {
-        return flagged.contains(v);
-    }
-
-    public Iterable<Ver<T>> path(T v) {
-        if (!ifHas(new Ver<>(v))) return null;
-        LinkedList<Ver<T>> list = new LinkedList<>();
-        Stream.iterate(new Ver<>(v), i -> i != sou, i -> verMap.get(i)).forEach(list::push);
-        list.push(sou);
-
-        return list;
-    }
-
-    public int getCounter() {
-        return counter;
-    }
-}
-
-class weiGraph<T> {
-    private boolean undir;
-    public ArrayList<Ver<T>> verMap = new ArrayList<>();
-    private Map<Ver, List<Find<Ver>>> map = new Map<Ver, List<Find<Ver>>>() {
-        @Override
-        public int size() {
-            return 0;
         }
 
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            return false;
-        }
-
-        @Override
-        public boolean containsValue(Object value) {
-            return false;
-        }
-
-        @Override
-        public List<Find<Ver>> get(Object key) {
-            return null;
-        }
-
-        @Override
-        public List<Find<Ver>> put(Ver key, List<Find<Ver>> value) {
-            return null;
-        }
-
-        @Override
-        public List<Find<Ver>> remove(Object key) {
-            return null;
-        }
-
-        @Override
-        public void putAll(Map<? extends Ver, ? extends List<Find<Ver>>> m) {
-
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public Set<Ver> keySet() {
-            return null;
-        }
-
-        @Override
-        public Collection<List<Find<Ver>>> values() {
-            return null;
-        }
-
-        @Override
-        public Set<Entry<Ver, List<Find<Ver>>>> entrySet() {
-            return null;
-        }
-    };
-    public weiGraph() {
-        this.undir = true;
     }
 
-    public weiGraph(boolean undir) {
-        this.undir = undir;
-    }
+    class Find<T> {
+        protected int counter;
+        protected Set<Ver<T>> flagged;
+        protected Map<Ver<T>, Ver<T>> verMap;
+        protected final Ver<T> sou;
 
-    public void VerAdd(T ver) {
-        verMap.add(new Ver<>(ver));
-    }
-
-    public void EdgeAdd(T sou, T dest, double wei) {
-        if (Ver0(sou)) {
-            VerAdd(sou);
-        }
-        if (Ver0(dest))
-            VerAdd(dest);
-
-        if (Edge0(sou, dest)
-                || sou.equals(dest) || !verMap.contains(new Ver<>(sou))) return;
-
-        verMap.get(verMap.indexOf(new Ver<>(sou))).addAdjVer(new Ver<>(dest), wei);
-
-
-        if (undir)
-            verMap.get(verMap.indexOf(new Ver<>(dest))).addAdjVer(new Ver<>(sou), wei);
-    }
-
-    public int getVerCount() {
-        return verMap.size();
-    }
-
-    public int getEdgeCount() {
-        int counter = 0;
-
-        for (Ver<T> to : verMap) {
-            counter += to.getAdj().size();
+        public Find(Ver<T> sou) {
+            this.sou = sou;
+            flagged = new HashSet<>();
+            verMap = new HashMap<>();
         }
 
-        if (undir)counter /= 2;
+        public boolean ifHas(Ver<T> v) {
+            return flagged.contains(v);
+        }
 
-        return counter;
+        public Iterable<Ver<T>> path(T v) {
+            if (!ifHas(new Ver<>(v))) return null;
+            LinkedList<Ver<T>> list = new LinkedList<>();
+            Stream.iterate(new Ver<>(v), i -> i != sou, i -> verMap.get(i)).forEach(list::push);
+            list.push(sou);
+
+            return list;
+        }
+
+        public int getCounter() {
+            return counter;
+        }
+    }
+
+    class weiGraph<T> {
+        private final boolean undir;
+        public ArrayList<Ver<T>> verMap = new ArrayList<>();
+        public weiGraph() {
+            this.undir = true;
+        }
+
+        public weiGraph(boolean undir) {
+            this.undir = undir;
+        }
+
+        public void VerAdd(T ver) {
+            verMap.add(new Ver<>(ver));
+        }
+
+        public void EdgeAdd(T sou, T dest, double wei) {
+            if (Ver0(sou)) {
+                VerAdd(sou);
+            }
+            if (Ver0(dest))
+                VerAdd(dest);
+
+            if (Edge0(sou, dest)
+                    || sou.equals(dest) || !verMap.contains(new Ver<>(sou))) return;
+
+            verMap.get(verMap.indexOf(new Ver<>(sou))).addAdjVer(new Ver<>(dest), wei);
+
+
+            if (undir)
+                verMap.get(verMap.indexOf(new Ver<>(dest))).addAdjVer(new Ver<>(sou), wei);
+        }
+
+        public int getVerCount() {
+            return verMap.size();
+        }
+
+        public int getEdgeCount() {
+            int counter = 0;
+
+            for (Ver<T> to : verMap) {
+                counter += to.getAdj().size();
+            }
+
+            if (undir)counter /= 2;
+
+            return counter;
+        }
+
+
+        public boolean Ver0(T v) {
+            if (!verMap.contains(new Ver<>(v))) return true;
+            else return false;
+        }
+
+        public boolean Edge0(T source, T dest) {
+            return !Ver0(source) && verMap.get(verMap.indexOf(new Ver<>(source))).getAdj().containsKey(new Ver<>(dest));
+        }
     }
 
 
-    public boolean Ver0(T v) {
-        if (!verMap.contains(new Ver<>(v))) return true;
-        else return false;
-    }
 
-    public boolean Edge0(T source, T dest) {
-        return !Ver0(source) && verMap.get(verMap.indexOf(new Ver<>(source))).getAdj().containsKey(new Ver<>(dest));
-    }
-    class Main {
+    public class Main {
 
 
 
 
-        public void main(String[] args) {
+        public static void main(String[] args) {
             weiGraph<String> graph = new weiGraph<>(true);
+
+
 
             graph.EdgeAdd("Almaty", "Astana", 2.1);
             graph.EdgeAdd("Almaty", "Shymkent", 7.2);
@@ -228,9 +173,9 @@ class weiGraph<T> {
             graph.EdgeAdd("Astana", "Kostanay", 3.5);
             graph.EdgeAdd("Shymkent", "Kyzylorda", 5.4);
 
-            System.out.println("Dijkstra:");
-            Find<String> djk = new DepthFirstFind<>(graph, "Almaty");
-            outputPath(djk, "Kyzylorda");
+//        System.out.println("Dijkstra:");
+//        Find<String> djk = new DijkstraSearch<>(graph, "Almaty");
+//        outputPath(djk, "Kyzylorda");
 
 //        System.out.println("DFS:");
 //        Search<String> dfs = new DepthFirstSearch<>(graph, "Almaty");
@@ -243,13 +188,12 @@ class weiGraph<T> {
 //        outputPath(bfs, "Kyzylorda");
         }
 
-        public void outputPath(Find<String> search, String key) {
-            for (String v : search.ifHas(key)) {
+        public static void outputPath(Find<String> search, String key) {
+            for (Ver<String> v : search.path(key)) {
                 System.out.print(v + " -> ");
             }
         }
     }
-}
 
 
 
